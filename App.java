f4f0c5842317b2e04cb205dbd6237f10353b2058
prasadmv-collab <prasadmvmsdpsd@gmail.com>
2026-04-15 13:33:20 +0530

    Update App.java

--- App.java
+++ App.java
@@ -5,48 +5,29 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-public class App {
-    public static void main(String[] args) {
-
+public class App 
+{
+    public static void main(String[] args)
+    {
+        //  Add this block
         ChromeOptions options = new ChromeOptions();
-
-        options.addArguments("--headless=new");
+        options.addArguments("--headless=new");   // required for Jenkins
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--disable-gpu");
-        options.addArguments("--window-size=1920,1080");
-        options.addArguments("--remote-allow-origins=*");
 
-        // Stability flags for Linux/VM
-        options.addArguments("--disable-software-rasterizer");
-        options.addArguments("--disable-extensions");
-        options.addArguments("--disable-background-networking");
-        options.addArguments("--disable-sync");
-        options.addArguments("--metrics-recording-only");
-        options.addArguments("--disable-default-apps");
-        options.addArguments("--no-first-run");
-        options.addArguments("--disable-notifications");
-
-        // Optional (safe)
-        options.setBinary("/usr/bin/google-chrome");
-
+        //  Replace driver creation
         WebDriver driver = new ChromeDriver(options);
 
-        try {
-            driver.get("https://www.saucedemo.com/");
+        driver.get("https://www.saucedemo.com/");
+        
+        //  maximize may fail in headless, so optional
+        // driver.manage().window().maximize();
 
-            driver.findElement(By.id("user-name")).sendKeys("standard_user");
-            driver.findElement(By.id("password")).sendKeys("secret_sauce");
-            driver.findElement(By.id("login-button")).click();
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        driver.findElement(By.id("login-button")).click();
 
-            System.out.println("Page Title: " + driver.getTitle());
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            if (driver != null) {
-                driver.quit();
-            }
-        }
+        // good practice
+        driver.quit();
     }
 }
