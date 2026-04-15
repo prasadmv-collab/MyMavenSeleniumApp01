48a6382cfd45157bfadcb42c6d9a0a8cc5c63351
prasadmv-collab <prasadmvmsdpsd@gmail.com>
2026-04-15 13:01:00 +0530

    Update App.java

--- App.java
+++ App.java
@@ -5,29 +5,32 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-public class App 
-{
-    public static void main(String[] args)
-    {
-        //  Add this block
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless=new");   // required for Jenkins
-        options.addArguments("--no-sandbox");
-        options.addArguments("--disable-dev-shm-usage");
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
 
-        //  Replace driver creation
+        // Configure Chrome for headless execution
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new");       // modern headless mode
+        options.addArguments("--no-sandbox");         // required in Jenkins/Linux
+        options.addArguments("--disable-dev-shm-usage"); // prevents crashes
+        options.addArguments("--disable-gpu");
+        options.addArguments("--window-size=1920,1080"); // optional but useful
+
         WebDriver driver = new ChromeDriver(options);
 
-        driver.get("https://www.saucedemo.com/");
-        
-        //  maximize may fail in headless, so optional
-        // driver.manage().window().maximize();
+        try {
+            driver.get("https://www.saucedemo.com/");
 
-        driver.findElement(By.id("user-name")).sendKeys("standard_user");
-        driver.findElement(By.id("password")).sendKeys("secret_sauce");
-        driver.findElement(By.id("login-button")).click();
+            driver.findElement(By.id("user-name")).sendKeys("standard_user");
+            driver.findElement(By.id("password")).sendKeys("secret_sauce");
+            driver.findElement(By.id("login-button")).click();
 
-        // good practice
-        driver.quit();
+            System.out.println("Page Title: " + driver.getTitle());
+        } finally {
+            driver.quit(); // always close browser
+        }
     }
 }
